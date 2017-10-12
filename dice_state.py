a38ddb0c0fd017aabe6fc966953c5a8fc34f0f33
Zhiyuan Li <zl7904@g.rit.edu>
2017-10-12 15:58:53 -0400

    dice roll

--- dice_state.py
+++ dice_state.py
@@ -2,16 +2,52 @@
 
 import numpy as np
 
-class dice_state():
-	def __init__(self,grid,location,top,north,east):
+class dice():
+	def __init__(self,maze,location,top,north,east):
 		self.location=location
 		self.top=top
 		self.north=north
 		self.east=east
 		self.parent=[]
-		self.children=self.get_children(grid)
+		self.children=[]
 	
-	def get_children(self,grid):
-		
-		
-	def 
+	def get_children(self,maze):
+		children=[]
+		max_row=len(maze)
+		max_col=len(maze[0])
+		offsets=[[1,0],[-1,0],[0,1],[0,-1]]
+		for offset in offsets:
+			new_row=offset[0]+self.location[0]
+			new_col=offset[1]+self.location[1]
+			if new_row<0 or new_row>=max_row or new_col<0 or new_col>=max_col:
+				continue
+			new_location=[new_row,new_col]
+			[top,north,east]=self.roll_dice(offset)
+			if top!=6:
+				child=dice(maze,new_location,top,north,east)
+				children.append(child)
+		return children
+
+	def roll_dice(self,offset):
+		top=0
+		north=0
+		east=0
+		if offset==[-1,0]:
+			top=7-self.north
+			north=self.top
+			east=self.east
+		elif offset==[1,0]:
+			top=self.north
+			north=7-self.top
+			east=self.east
+		elif offset==[0,1]:
+			top=7-self.east
+			north=self.north
+			east=self.top
+		elif offset==[0,-1]:
+			top=self.east
+			north=self.north
+			east=7-self.top
+		else:
+			print('unknow offset')
+		return [top,north,east]
--- testing.py
+++ testing.py
@@ -1,5 +1,17 @@
 from utils import *
+from dice_state import *
 
 print(euclidean([0,0],[1,1]))
 
-print(read_maze('puzzle/puzzle1.txt'))
+maze=read_maze('puzzle/puzzle2.txt')
+
+print(maze)
+
+print('test dice roll')
+start_location=dice(maze,[1,1],1,2,3)
+print(start_location.location,start_location.top,start_location.north,start_location.east)
+print()
+
+children=start_location.get_children(maze)
+for child in children:
+	print(child.location,child.top,child.north,child.east)
