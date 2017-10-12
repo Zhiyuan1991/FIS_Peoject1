1935982b1c06674588b009379f4a6a3d1d3910e6
sahilgupta221 <sahil15057@iiitd.ac.in>
2017-10-12 18:30:08 -0400

    added manhatten distance function

--- utils.py
+++ utils.py
@@ -7,5 +7,11 @@ def read_maze(file_name):
 	return mazeList
 
 def euclidean(locationA, locationB):
-	return ((locationA[0]-locationB[0])**2+(locationA[1]-locationB[1])**2)**0.5
+	return ((locationA[0]-locationB[0])**2+(locationA[1]-
+											ocationB[1])**2)**0.5
 
+def manhattan_distance(locationA, locationB):
+	return (abs(locationA[0]-locationB[0])+abs(locationA[1]-locationB[1]))
+
+def orientation_manhattandistance(locationA, locationB):
+	return (abs(locationA[0]-locationB[0])+abs(locationA[1]-locationB[1]))
