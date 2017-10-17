632138244d200f33401d94ec801b4cd8f6e260bc
sahilgupta221 <sahilgupta221@gmail.com>
2017-10-16 20:10:20 -0400

    Update function again

--- utils.py
+++ utils.py
@@ -23,6 +23,32 @@ def manhattan_distance(locationA, locationB):
 	return (abs(locationA[0]-locationB[0])+abs(locationA[1]-locationB[1]))
 
 
+#define functions
+import math
+
+import copy
+
+def read_maze(file_name):
+	f=open(file_name,'r')
+	readLines = f.readlines()
+	mazeList = [list(i.strip()) for i in readLines]
+	return mazeList
+
+def display_maze(maze, dice=None):
+	maze_copy=copy.deepcopy(maze)
+	if dice:
+		location=dice.location
+		maze_copy[location[0]][location[1]]=str(dice.top)
+	for line in maze_copy:
+		print(line)
+
+def euclidean(locationA, locationB):
+	return ((locationA[0]-locationB[0])**2+(locationA[1]-locationB[1])**2)**0.5
+
+def manhattan_distance(locationA, locationB):
+	return (abs(locationA[0]-locationB[0])+abs(locationA[1]-locationB[1]))
+
+
 def orientation_manhattan_distance(locationA, locationB,currenttop,goaltop):
     current_x, current_y = locationA[0], locationA[1]
     goal_x, goal_y = locationB[0], locationB[1]
@@ -44,10 +70,12 @@ def orientation_manhattan_distance(locationA, locationB,currenttop,goaltop):
     if x_steps > 0 and y_steps == 0 and is_target_goal_top: #Case 4: in line with goal state in horizontal direction with goal state configuration on top. Now one can rotate in up/down direction, make x_steps moves and reverse rotate it.
         return manhattan + 2
 
-    if dx > 0 and dy > 0 and is_target_goal_top: # [1, inf), [1, inf), 0
+    if x_steps > 0 and y_steps > 0 and is_target_goal_top: # [1, inf), [1, inf), 0
         return manhattan
 
-    if dx > 0 and dy > 0 and not is_target_goal_top: # [1, inf), [1, inf), 1
+    if x_steps > 0 and y_steps > 0 and not is_target_goal_top: # [1, inf), [1, inf), 1
         return manhattan +4
 
     return manhattan # default to manhattan
+
+
